93015fdbd7871c351339669189204631354a2d6b
Srilashman <srilashman.sg@gmail.com>
2025-02-04 15:46:44 +0800

    Change js form validation to more strict

--- js/form_validate.js
+++ js/form_validate.js
@@ -1,16 +1,16 @@
 function validateSignUpForm() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var cfmPassword = document.getElementById("confirm-password").value;
-    var email =  document.getElementById("email").value;
-    var errMsg = "";
-    var missingFields = [];
-    if (username.trim() == "") missingFields.push("username");
-    if (email.trim() == "") missingFields.push("email");
-    if (password.trim() == "") missingFields.push("password");
-    if (cfmPassword.trim() == "" && password !== "") missingFields.push("confirm password");
-    if (cfmPassword && password && cfmPassword !== password) missingFields.push("passwords don't match");
-    if (missingFields.length != 0 && missingFields[0] == "passwords don't match") { // so that "Enter your passwords don't match" changes to below
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let cfmPassword = document.getElementById("confirm-password").value;
+    let email =  document.getElementById("email").value;
+    let errMsg = "";
+    let missingFields = [];
+    if (username.trim() === "") missingFields.push("username");
+    if (email.trim() === "") missingFields.push("email");
+    if (password.trim() === "") missingFields.push("password");
+    if (cfmPassword.trim() === "" && password.trim() !== "") missingFields.push("confirm password");
+    if (cfmPassword.trim() && password.trim() && cfmPassword !== password) missingFields.push("passwords don't match");
+    if (missingFields.length !== 0 && missingFields[0] === "passwords don't match") { // so that "Enter your passwords don't match" changes to below
         errMsg = "Passwords don't match";
         document.getElementById("error").innerText = errMsg;
     } else if (missingFields.length > 0) {
@@ -24,10 +24,10 @@ function validateSignUpForm() {
     return changeErrH();
 }
 function validateLoginForm() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var errMsg = "";
-    var missingFields = [];
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let errMsg = "";
+    let missingFields = [];
     if (username.trim() === "") missingFields.push("username");
     if (password.trim() === "") missingFields.push("password");
     if (missingFields.length > 0) {
