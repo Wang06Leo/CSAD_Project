ba84863a3b5aab747363331f3900c28bfd590a6a
Clarence00000 <engyongxun@gmail.com>
2025-02-05 19:03:39 +0800

    Fix minor menu ui issue during merging

--- success.php
+++ success.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Receipt Page</title>
 </head>
 <body>
-    <h1>Success Page</h1>
+    <!-- Header -->
+    <header>
+        <div class="logo">
+            <img id="cube" src="image/cube.png">
+            Steak <span class="box">Box</span>
+        </div>
+        <nav>
+            <a href="main.php">Home</a>
+            <a href="menu.php" class="head-order-button">Order Here</a>
+            <a href="login.php" class="head-order-button">Login</a>
+            <a href="#">
+                <div class="icon-container">
+                    <img src="image/Ellipse 1.png" alt="Circle" class="background-circle">
+                    <img src="image/Vector.png" alt="Cart Icon" class="cart-icon">
+                </div>
+            </a>
+        </nav>
+    </header>
 </body>
 </html>
