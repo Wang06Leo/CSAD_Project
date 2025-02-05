977c569f27856fe210cf27f446584a1107a3f25b
Srilashman <srilashman.sg@gmail.com>
2025-02-05 17:03:36 +0800

    Fix bug where if localhost port exists, getBaseUrl() doesnt work, add description of item and gst

--- src/php/checkout.php
+++ src/php/checkout.php
@@ -2,6 +2,9 @@
 session_start();
 require "../vendor/autoload.php";
 require "secrets.php";
+/**
+ * @var string $stripeSecretKey
+ */
 \Stripe\Stripe::setApiKey($stripeSecretKey);
 // Get JSON input
 $json_data = json_decode($_POST['checkout_data'], true);
@@ -13,31 +16,39 @@ if (count($json_data) === 0) {
     header("Location: ../../menu.php");
 }
 foreach ($json_data as $item) {
+    $item['size'][0] = strtoupper($item['size'][0]);
     $items[] = [
         "quantity" => $item["quantity"],
         "price_data" => [
             "currency" => "sgd",
             "unit_amount" => $item["price"] * 100,
             "product_data" => [
-                "name" => $item["title"]
+                "name" => $item["title"],
+                "tax_code" => "txcd_20030000",
+                "description" => "Size: " . $item['size']
             ]
         ]
     ];
 }
 function getBaseUrl() {
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
-
-    return "$protocol://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    return "$protocol://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 }
-$checkout_session = \Stripe\Checkout\Session::create([
-    "mode" => "payment",
-    "success_url" => dirname(dirname(dirname(getBaseUrl()))) . "/success.php",
-    "cancel_url" => dirname(dirname(dirname(getBaseUrl()))). "/menu.php",
-    //'payment_method_types' => $paymentTypes,
-    "line_items" => [
-        $items
-    ]
-]);
-http_response_code(303);
-header("Location: " . $checkout_session->url);
-exit();
+try {
+    $checkout_session = \Stripe\Checkout\Session::create([
+        "mode" => "payment",
+        "success_url" => dirname(getBaseUrl(), 3) . "/success.php",
+        "cancel_url" => dirname(getBaseUrl(), 3) . "/menu.php",
+        "line_items" => [
+            $items
+        ],
+        'automatic_tax' => ['enabled' => true],
+    ]);
+    
+    //$_SESSION['session_id']  = $checkout_session->id;
+    http_response_code(303);
+    header("Location: " . $checkout_session->url);
+    exit();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
