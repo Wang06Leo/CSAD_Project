e3c5ceeec1011a0bd4a5a8283fdea16be13e6138
Clarence00000 <engyongxun@gmail.com>
2025-02-09 18:03:38 +0800

    change title of admin page

--- admin/panel.php
+++ admin/panel.php
@@ -54,7 +54,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Page</title>
     <style>
         /* General Styling */
         body {
