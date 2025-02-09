66c7d54867abcd2e935006026923855fd05cc17b
Srilashman <srilashman.sg@gmail.com>
2025-02-09 14:09:11 +0800

    Fix logout bug

--- src/php/logout.php
+++ src/php/logout.php
@@ -1,5 +1,22 @@
 <?php
+function getBaseUrl() {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+    return "$protocol://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+}
 session_start();
+if (isset($_SESSION['username']) && isset($_SESSION['email']) && isset($_SESSION['points']) && isset($_SESSION['admin_user'])) {
+    if ($_SERVER['HTTP_REFERER'] === (dirname(getBaseUrl(), 3) . "/admin/panel.php")) {
+        unset($_SESSION['admin_user']);
+        header("Location: ../../admin/index.php");
+        exit();
+    } else {
+        unset($_SESSION['username']);
+        unset($_SESSION['email']);
+        unset($_SESSION['points']);
+        header("Location: ../../login.php"); // Redirect to login page
+        exit();
+    }
+}
 if (isset($_SESSION['username']) && isset($_SESSION['email']) && isset($_SESSION['points'])) {
     unset($_SESSION['username']);
     unset($_SESSION['email']);
@@ -7,7 +24,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['email']) && isset($_SESSION
 }
 if (isset($_SESSION['admin_user'])) {
     unset($_SESSION['admin_user']);
-    header("Location: ../../admin/panel.php");
+    header("Location: ../../admin/index.php");
     exit();
 }
 header("Location: ../../login.php"); // Redirect to login page
